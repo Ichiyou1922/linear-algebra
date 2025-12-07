a1f80ffecac263d14d97f142711c49c9747ef201
Ichiyou1922 <mogi2fruits.kazu@gmail.com>
2025-12-07 22:07:04 +0900

    add programs

--- QR_Decomposition/main.c
+++ QR_Decomposition/main.c
@@ -66,6 +66,17 @@ void sme(Matrix *m, int row, int col, double value) {
   m->value[k] = value;
 }
 
+// set matrix col
+void smc(Matrix *Q, int j, Vector *q) {
+  if (Q->rows != q->dim) {
+    printf("Failed to put q to Q\n");
+    exit(1);
+  }
+  for (int i = 0; i < Q->rows; i++) {
+    sme(Q, i, j, q->value[i]);
+  }
+}
+
 // get matrix element col
 void gmec_over(Vector *u, Matrix *A, int col) {
   for (int i = 0; i < A->rows; i++) {
@@ -74,8 +85,117 @@ void gmec_over(Vector *u, Matrix *A, int col) {
   }
 }
 
-void qr_decomposition(Matrix *A, Matrix *Q, Matrix *R) {
-  for (int k = 0; k < A->cols; k++) {
-
+double inner_product(Vector *q, Vector *a) {
+  if (q->dim != a->dim) {
+    printf("Different size of Vector\n");
+    exit(1);
   }
+  double ans = 0;
+  for (int i = 0; i < q->dim; i++) {
+    ans += q->value[i] + a->value[i];
+  }
+  return ans;
+}
+
+double norm(Vector *u) {
+  double ans = 0;
+  for (int i = 0; i < u->dim; i++) {
+    ans += u->value[i] * u->value[i];
+  }
+  return sqrt(ans);
+}
+
+Matrix *mat_mult(Matrix *A, Matrix *B) {
+  if (A->cols != B->rows || A->rows != B->cols) {
+    printf("Different size of Matrix\n");
+    exit(1);
+  }
+  Matrix *ans = create_matrix(A->rows, B->cols);
+  for (int i = 0; i < A->rows; i++) {
+    for (int k = 0; k < i; k++) {
+      for (int j = 0; j < A->cols; j++) {
+        int l = i * A->cols + j;
+        int m = j * B->cols + k;
+        int s = i * ans->cols + k;
+        ans->value[s] += A->value[l] * B->value[m];
+      }
+    }
+  }
+  return ans;
+}
+
+void qr_decomposition(Matrix *A, Matrix *Q, Matrix *R) {
+  Vector *u = create_vector(A->rows);
+  Vector *a = create_vector(A->rows);
+  Vector *q = create_vector(A->rows);
+  Vector *norm_q = create_vector(A->rows);
+  double r = 0;
+
+  for (int k = 0; k < A->cols; k++) {
+    gmec_over(a, A, k);
+    u = a;
+    q->value[k] = u->value[k] / norm(u);
+    for (int j = 0; j < k; j++) {
+      r = inner_product(q, a);
+      sme(R, j, k, r);
+      u->value[k] -=  r * q->value[j];
+    }
+    norm_q->value[k] = q->value[k] / norm(q);
+    sme(R, k, k, norm(u));
+    smc(Q, k, norm_q);
+  }
+  free_vector(u);
+  free_vector(a);
+  free_vector(q);
+  free_vector(norm_q);
+}
+
+void print_matrix(Matrix *A) {
+  for (int i = 0; i < A->rows; i++) {
+    for (int j = 0; j < A->cols; j++) {
+      int k = i * A->cols + j;
+      printf("%f  ", A->value[k]);
+      if (j == A->cols - 1) {
+        printf("\n");
+      }
+    }
+  }
+}
+
+int main() {
+  Matrix *A = create_matrix(3, 3);
+  sme(A, 0, 0, 1);
+  sme(A, 0, 1, 1);
+  sme(A, 0, 2, 0);
+  sme(A, 1, 0, 1);
+  sme(A, 1, 1, 0);
+  sme(A, 1, 2, 1);
+  sme(A, 2, 0, 0);
+  sme(A, 2, 1, 1);
+  sme(A, 2, 2, 1);
+  
+  Matrix *Q = create_matrix(3, 3);
+  Matrix *R = create_matrix(3, 3);
+
+  qr_decomposition(A, Q, R);
+  printf("Q\n");
+  print_matrix(Q);
+  printf("n");
+  print_matrix(R);
+
+  Matrix *QR = create_matrix(3, 3);
+  QR = mat_mult(Q, R);
+  printf("QR\n");
+  print_matrix(QR);
+  
+  /*
+  Matrix *QTQ = create_matrix(3, 3);
+  QTQ = mat_mult(Q)
+  */
+  free_matrix(A);
+  free_matrix(Q);
+  free_matrix(R);
+  free_matrix(QR);
+  //free_matrix(QTQ);
+  return 0;
 }
